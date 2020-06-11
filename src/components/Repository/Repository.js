60a91a70efcbfa71c526445363508f120a944992
Ricardo Weiss <ricardohsweiss@gmail.com>
2020-06-11 20:03:45 -0300

    more destructuring applied on Repository.js

--- src/components/Repository/Repository.js
+++ src/components/Repository/Repository.js
@@ -3,12 +3,13 @@ import React from "react";
 const starImage = require('../../images/star.png');
 
 const Repository = ({ repositorio }) => {
+    const { html_url, name, stargazers_count } = repositorio;
     return (
         <div className="repositories__repository">
-            <a href={repositorio.html_url} target="_blank" rel="noopener noreferrer">
-                <p className="repository__name link">{repositorio.name}</p>
+            <a href={html_url} target="_blank" rel="noopener noreferrer">
+                <p className="repository__name link">{name}</p>
             </a>
-            <p className="repository__star-count"> {repositorio.stargazers_count} <img className="repository__star" src={starImage} alt=""/></p>
+            <p className="repository__star-count"> {stargazers_count} <img className="repository__star" src={starImage} alt=""/></p>
         </div>
     )
 }
