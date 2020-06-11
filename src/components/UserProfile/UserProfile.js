18176b9b5f8bb560f992821888fdc9c494062a11
Ricardo Weiss <ricardohsweiss@gmail.com>
2020-06-11 19:14:41 -0300

    UserProfile.js - destructuring applied

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -4,23 +4,20 @@ import Follows from "../Follows/Follows";
 
 const circuitImage = require('../../images/data-circuit-image.png');
 
-const UserProfile = (props) => {
+const UserProfile = ({ info }) => {
     return (
-
             <div className="user-profile__container">
-                <img className="user-profile__image" src={props.info.avatar_url} alt=""/>
+                <img className="user-profile__image" src={info.avatar_url} alt=""/>
                 <div className="user-profile__info-container">
-                    <p className="user-profile__info"><span className="title--whitebold">Name:</span> <br/>{props.info.name}</p>
+                    <p className="user-profile__info"><span className="title--whitebold">Name:</span> <br/>{info.name}</p>
                     <p className="user-profile__info"><span
-                        className="title--whitebold">Bio:</span><br/> {props.info.bio ? props.info.bio : 'no bio'} </p>
+                        className="title--whitebold">Bio:</span><br/> {info.bio ? info.bio : 'no bio'} </p>
                     <p className="user-profile__info"><span
-                        className="title--whitebold">Email:</span><br/> {props.info.email ? props.info.email : 'no email'}</p>
+                        className="title--whitebold">Email:</span><br/> {info.email ? info.email : 'no email'}</p>
                 </div>
-                <Follows info={props.info}/>
+                <Follows info={info}/>
                 <img className="user-profile__decoration" src={circuitImage} alt=""/>
             </div>
-
-
     )
 }
 
