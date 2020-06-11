c035a9397f658109a6f67c9e24aed2a340e1f6dc
Ricardo Weiss <ricardohsweiss@gmail.com>
2020-06-11 19:11:27 -0300

    Repository.js star image 'require' now stored in a constant, destructuring applied too

--- src/components/Repositories/Repositories.js
+++ src/components/Repositories/Repositories.js
@@ -2,10 +2,8 @@ import React from "react";
 import Repository from "../Repository/Repository";
 
 const Repositories =({ repo }) =>
-
             repo.sort((a, b) => (a.stargazers_count > b.stargazers_count) ? -1 : 1).map((repositorio) => {
                 return (<Repository repositorio={repositorio} key={repositorio.id}/>)
             })
-
 
 export default Repositories;
--- src/components/Repository/Repository.js
+++ src/components/Repository/Repository.js
@@ -1,13 +1,14 @@
 import React from "react";
 
+const starImage = require('../../images/star.png');
 
-const Repository = (props) => {
+const Repository = ({ repositorio }) => {
     return (
         <div className="repositories__repository">
-            <a href={props.repositorio.html_url} target="_blank" rel="noopener noreferrer">
-                <p className="repository__name link">{props.repositorio.name}</p>
+            <a href={repositorio.html_url} target="_blank" rel="noopener noreferrer">
+                <p className="repository__name link">{repositorio.name}</p>
             </a>
-            <p className="repository__star-count"> {props.repositorio.stargazers_count} <img className="repository__star" src={require('../../images/star.png')} alt=""/></p>
+            <p className="repository__star-count"> {repositorio.stargazers_count} <img className="repository__star" src={starImage} alt=""/></p>
         </div>
     )
 }
